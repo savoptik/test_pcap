b1f06823b51c7c5c0388794aaee9107775871539
savoptik <sav70792@gmail.com>
2019-08-30 13:24:45 +0300

    Скрипт компиляции и запуска переодаптирован для использования с новой версией

--- Compile_and_run_Script.sh
+++ Compile_and_run_Script.sh
@@ -6,9 +6,9 @@
 #  Created by Артём Семёнов on 24.08.2019.
 #  
 
-scp ./main.c ubunta:~/project/github/pcap-master/
-if ssh ubunta "cd ~/project/github/pcap-master/; make"
-then ssh ubunta "cd ~/project/github/pcap-master/; ./test_pcap --vdev 'net_pcap0,rx_pcap=test_tcp.pcap,tx_pcap=test_tcp_out.pcap' --no-huge" 1>log.txt
+scp ./main.c ubunta:~/project/github/test_pcap/
+if ssh ubunta "cd ~/project/github/test_pcap/; make"
+then ssh ubunta "cd ~/project/github/test_pcap/; ./test_pcap --vdev 'net_pcap0,rx_pcap=test_tcp.pcap,tx_pcap=test_tcp_out.pcap' --no-huge" 1>log.txt
 else echo "error"
 fi
 
